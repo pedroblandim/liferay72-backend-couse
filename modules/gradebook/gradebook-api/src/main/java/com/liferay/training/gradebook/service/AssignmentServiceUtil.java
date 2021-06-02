b26b627ac092ac3c9ee1d369bce247baf0057e76
pedro.blandim <pedro.blandim@gmail.com>
2021-06-02 13:02:54 -0300

    Initial commit

---
+++ modules/gradebook/gradebook-api/src/main/java/com/liferay/training/gradebook/service/AssignmentServiceUtil.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.training.gradebook.service;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * Provides the remote service utility for Assignment. This utility wraps
+ * <code>com.liferay.training.gradebook.service.impl.AssignmentServiceImpl</code> and is an
+ * access point for service operations in application layer code running on a
+ * remote server. Methods of this service are expected to have security checks
+ * based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author Pedro Blandim
+ * @see AssignmentService
+ * @generated
+ */
+public class AssignmentServiceUtil {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to <code>com.liferay.training.gradebook.service.impl.AssignmentServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	 * Returns the OSGi service identifier.
+	 *
+	 * @return the OSGi service identifier
+	 */
+	public static String getOSGiServiceIdentifier() {
+		return getService().getOSGiServiceIdentifier();
+	}
+
+	public static AssignmentService getService() {
+		return _serviceTracker.getService();
+	}
+
+	private static ServiceTracker<AssignmentService, AssignmentService>
+		_serviceTracker;
+
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(AssignmentService.class);
+
+		ServiceTracker<AssignmentService, AssignmentService> serviceTracker =
+			new ServiceTracker<AssignmentService, AssignmentService>(
+				bundle.getBundleContext(), AssignmentService.class, null);
+
+		serviceTracker.open();
+
+		_serviceTracker = serviceTracker;
+	}
+
+}
