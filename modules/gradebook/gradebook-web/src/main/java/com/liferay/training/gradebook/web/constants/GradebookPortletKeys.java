67079c3518efecdb23a588dd9495f9b48a5cd7c9
pedro.blandim <pedro.blandim@gmail.com>
2021-06-07 11:48:52 -0300

    Make adjusts to generated MVC portlet module - Declare dependencies - Change portlet properties - Change portlet name - Add portlet language properties

--- modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/constants/GradebookPortletKeys.java
+++ modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/constants/GradebookPortletKeys.java
@@ -6,6 +6,6 @@ package com.liferay.training.gradebook.web.constants;
 public class GradebookPortletKeys {
 
 	public static final String GRADEBOOK =
-		"com_liferay_training_gradebook_web_GradebookPortlet";
+		"com_liferay_training_gradebook_web_portlet_GradebookPortlet"; // fully qualified name of the portlet (best practices)
 
 }
