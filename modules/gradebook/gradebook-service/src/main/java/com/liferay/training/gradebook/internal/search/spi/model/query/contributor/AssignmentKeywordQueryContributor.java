e0bd048ad06c4a54627ff50d4b5d90a1157c5ca7
pedro.blandim <pedro.blandim@gmail.com>
2021-06-25 13:51:00 -0300

    Integrate with Portal Search

---
+++ modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/internal/search/spi/model/query/contributor/AssignmentKeywordQueryContributor.java
@@ -0,0 +1,44 @@
+package com.liferay.training.gradebook.internal.search.spi.model.query.contributor;
+
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.search.query.QueryHelper;
+import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
+import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * The Keyword Query Contributor contributes model-specific 
+* clauses to the ongoing search query
+* @author pedro
+*
+*/
+@Component(
+    immediate = true,
+    property = "indexer.class.name=com.liferay.training.gradebook.model.Assignment",
+    service = KeywordQueryContributor.class
+)
+public class AssignmentKeywordQueryContributor
+    implements KeywordQueryContributor {
+
+    @Override
+    public void contribute(
+        String keywords, BooleanQuery booleanQuery,
+        KeywordQueryContributorHelper keywordQueryContributorHelper) {
+
+        SearchContext searchContext =
+            keywordQueryContributorHelper.getSearchContext();
+
+        queryHelper.addSearchLocalizedTerm(
+            booleanQuery, searchContext, Field.DESCRIPTION, false);
+        queryHelper.addSearchLocalizedTerm(
+            booleanQuery, searchContext, Field.TITLE, false);
+    }
+
+    @Reference
+    protected QueryHelper queryHelper;
+
+}
